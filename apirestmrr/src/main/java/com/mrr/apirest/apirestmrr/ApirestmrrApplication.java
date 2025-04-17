db8828d872e5bbdd609ce937f50632316af9917f
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-04-17 19:49:59 +0200

    Initial configuration

---
+++ apirestmrr/src/main/java/com/mrr/apirest/apirestmrr/ApirestmrrApplication.java
@@ -0,0 +1,13 @@
+package com.mrr.apirest.apirestmrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestmrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestmrrApplication.class, args);
+	}
+
+}
---
+++ apirestmrr/src/test/java/com/mrr/apirest/apirestmrr/ApirestmrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrr.apirest.apirestmrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestmrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
