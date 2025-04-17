34cfbf9bc1649e3faee524d43e3efe783e784e86
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-04-17 21:38:07 +0200

    Created Entity

---
+++ apirestmrr/src/main/java/com/mrr/apirest/apirestmrr/Entities/Receta.java
@@ -0,0 +1,62 @@
+package com.mrr.apirest.apirestmrr.Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Receta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ElementCollection
+    private List<String> ingredients = new ArrayList<>();
+
+    @ElementCollection
+    private List<String> steps = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<String> steps) {
+        this.steps = steps;
+    }
+
+
+    
+
+}
